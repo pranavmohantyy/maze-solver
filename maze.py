70a3910e1c53d3e0a2dbe3add441e3e2ad7e499d
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-12 12:13:58 -0700

    add maze generation algorithm

--- maze.py
+++ maze.py
@@ -3,6 +3,7 @@ class Maze:
         self.width = width
         self.height = height
         self.grid = [['#' for _ in range(width)] for _ in range(height)]
+        self.visited = [[False for _ in range(width)] for _ in range(height)]
 
     def render(self):
         for row in self.grid:
@@ -12,7 +13,19 @@ class Maze:
         if 0 <= x < self.width and 0 <= y < self.height:
             self.grid[y][x] = '.'
 
+    def generate_maze(self, x=0, y=0):
+        self.visited[y][x] = True
+        directions = [(0, 2), (2, 0), (0, -2), (-2, 0)]
+        random.shuffle(directions)
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < self.width and 0 <= ny < self.height and not self.visited[ny][nx]:
+                self.set_passage(x + dx // 2, y + dy // 2)
+                self.generate_maze(nx, ny)
+
+import random
+
 maze = Maze(10, 10)
-maze.set_passage(1, 1)
-maze.set_passage(2, 1)
+maze.generate_maze()
 maze.render()
