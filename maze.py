24f1d16a6057559593a13e15405d6a32ccd6766f
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-13 11:14:03 -0700

    add start and end markers

--- maze.py
+++ maze.py
@@ -4,11 +4,22 @@ class Maze:
         self.height = height
         self.grid = [['#' for _ in range(width)] for _ in range(height)]
         self.visited = [[False for _ in range(width)] for _ in range(height)]
+        self.start = (0, 0)
+        self.end = (width - 1, height - 1)
 
     def render(self):
         print("\033[H\033[J", end='')
-        for row in self.grid:
-            print(''.join(row))
+        for y, row in enumerate(self.grid):
+            for x, cell in enumerate(row):
+                if (x, y) == self.start:
+                    print('\033[91mS\033[0m', end='')
+                elif (x, y) == self.end:
+                    print('\033[94mE\033[0m', end='')
+                elif self.visited[y][x]:
+                    print('\033[92m.\033[0m', end='')
+                else:
+                    print(cell, end='')
+            print()
 
     def set_passage(self, x, y):
         if 0 <= x < self.width and 0 <= y < self.height:
@@ -22,41 +33,5 @@ class Maze:
             nx, ny = x + dx * 2, y + dy * 2
             if 0 <= nx < self.width and 0 <= ny < self.height and not self.visited[ny][nx]:
                 self.set_passage(x + dx, y + dy)
-                self.generate_maze(nx, ny)
+                self.generate_maze(nx, ny, speed)
                 time.sleep(speed)
-
-    def solve_maze(self, start, end):
-        stack = [start]
-        while stack:
-            x, y = stack.pop()
-            if (x, y) == end:
-                return True
-            if self.visited[y][x]:
-                continue
-            self.visited[y][x] = True
-            for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < self.width and 0 <= ny < self.height and self.grid[ny][nx] == '.':
-                    stack.append((nx, ny))
-        return False
-
-if __name__ == '__main__':
-    import argparse
-    import random
-    import time
-
-    parser = argparse.ArgumentParser(description='Generate and solve mazes.')
-    parser.add_argument('--width', type=int, required=True, help='Width of the maze')
-    parser.add_argument('--height', type=int, required=True, help='Height of the maze')
-    parser.add_argument('--algo', choices=['bfs'], required=True, help='Algorithm to use for solving')
-    parser.add_argument('--animate', action='store_true', help='Animate the maze generation')
-    args = parser.parse_args()
-
-    maze = Maze(args.width, args.height)
-    maze.generate_maze(speed=0.1 if args.animate else 0)
-    maze.render()
-    if args.algo == 'bfs':
-        if maze.solve_maze((0, 0), (args.width - 1, args.height - 1)):
-            print("Maze solved!")
-        else:
-            print("No solution found.")
